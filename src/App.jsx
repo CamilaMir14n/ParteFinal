19f19dc72cbbf931283c189e9f14cd4a0648459f
CamilaMir14n <camila.mirian@aluno.ifsp.edu.br>
2025-07-05 21:17:02 -0300

    Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "../components/card"; // Corrigido para corresponder ao nome do
 const perfilData = {
   nome: 'Lucas Silva',
   idade: 6,
-  avatar: '/assets/avatar.png',
+  avatar: 'https://i.pinimg.com/736x/13/e7/9d/13e79dfaa493be4b88f62d057073d86b.jpg',
   historico: [
     { atividade: 'Cores e Formas', data: '20/06/2025' },
     { atividade: 'Jogo da Memória', data: '18/06/2025' },
@@ -16,17 +16,17 @@ const atividades = [
   {
     title: 'Cores e Formas',
     description: 'Ajude a criança a identificar cores e formas.',
-    image: '/assets/cores-e-formas.png'
+    image: 'https://i.pinimg.com/736x/6f/27/fd/6f27fdc55d8157d5d5a326ba6e63c27a.jpg'
   },
   {
     title: 'Jogo da Memória',
     description: 'Desenvolva a memória com figuras divertidas.',
-    image: '/assets/memoria.png'
+    image: 'https://i.pinimg.com/736x/32/0c/64/320c64ed31ae33bad13eded5dc804531.jpg'
   },
   {
     title: 'Complete a Palavra',
     description: 'Atividade de reforço da alfabetização.',
-    image: '/assets/palavras.png'
+    image: 'https://i.pinimg.com/736x/a7/d7/d5/a7d7d54a4d4393f0505b4c3ebf5aba91.jpg'
   }
 ];
 
@@ -34,19 +34,19 @@ const livros = [
   {
     titulo: 'Como Você Se Sente?',
     descricao: 'Um livro interativo para explorar emoções.',
-    imagem: '/assets/livro1.png',
+    imagem: 'https://i.pinimg.com/736x/73/ad/d9/73add98f0bff4c06e857fbe43a7c3e36.jpg',
     link: 'https://leiturinha.com.br/blog/livros-sobre-sentimentos-infantis/'
   },
   {
     titulo: 'O Monstro das Cores',
     descricao: 'Ajuda as crianças a identificar e nomear emoções.',
-    imagem: '/assets/livro2.png',
+    imagem: 'https://i.pinimg.com/736x/f7/83/e4/f783e421fa46a57dfd7d45125d2d9393.jpg',
     link: 'https://leiturinha.com.br/blog/livros-sobre-sentimentos-infantis/'
   },
   {
     titulo: 'A Raiva Não É Um Bicho-Papão',
     descricao: 'Ensina que sentir raiva é normal e como lidar com isso.',
-    imagem: '/assets/livro3.png',
+    imagem: 'https://i.pinimg.com/originals/54/98/c6/5498c63f5606637bbea83cde30acac6c.png',
     link: 'https://leiturinha.com.br/blog/livros-sobre-sentimentos-infantis/'
   }
 ];
